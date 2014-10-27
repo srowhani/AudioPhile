1ad0986bc18fcf69ec521d43b787c519d123f7f9
Seena Rowhani <seenarowhani95@gmail.com>
2014-10-27 00:11:55 -0400

    Update Player.js

--- js/Player.js
+++ js/Player.js
@@ -1,7 +1,8 @@
 define(function(require){
     var _songs = {};
     var _isPlaying = false;
-
+    var _self;
+    
     var Song = function(config){
         this.file = config.file;
         this.name = config.name;
@@ -14,38 +15,42 @@ define(function(require){
         _freq,
         _context;
 
-    (function init(){
-        _context  =  new window.AudioContext();
-        _source   = _context.createMediaElementSource(player);
-        _analyser = _context.createAnalyser();
-        _source.connect(_analyser); 
-        _analyser.connect(_context.destination); // connect the freq analyzer to the output
-        _freq = new Uint8Array(64);
-
-    })();
-
     var populateList = function(song){
         if(song.file.name in _songs) return;
 
         _songs[song.file.name] = song;
         song.element.setAttribute('class', 'song');
-        song.element.setAttribute('onclick', 'app.player[\'play\'](\'' + btoa(song.name) + '\')');
+        song.element.setAttribute('data-id', btoa(song.name));
+        song.element.addEventListener('click', function(e){
+            _self.play(e.srcElement);
+        }, false);
         var title = "<h4>".concat(song.name).concat("</h4>")
         var size = "<small>Size: ".concat(song.size).concat("mb</small>")
         song.element.innerHTML =  title.concat(size);
         songlist.appendChild(song.element);
     }
     return {
-        play : function(_name){
+
+        init : function(){
+            _context  =  new window.AudioContext();
+            _source   = _context.createMediaElementSource(player);
+            _analyser = _context.createAnalyser();
+            _source.connect(_analyser); 
+            _analyser.connect(_context.destination); // connect the freq analyzer to the output
+            _freq = new Uint8Array(64);
+            _self = this;
+            return this;
+        },
+        play : function(element){
+            var _name = element['dataset'].id;
             if(!_name) return;
+
             if( !(atob(_name) in _songs)) throw new Error("Unable to play song");
             else player.src = _songs[atob(_name)].dataurl;
-            [].slice.call(songlist.querySelectorAll('.song')).forEach(function(e){
-                if(e.onclick.toString().indexOf(_name) > -1)
-                    e.className="song playing";
-                else e.className="song";
+            [].slice.call(songlist.querySelectorAll('.song')).forEach(function(e){                    
+                e.className="song";
             });
-
+            element.className+=" playing";
             _freq = new Uint8Array(64); 
             player.play();
             _isPlaying = true;
