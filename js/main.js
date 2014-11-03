827b9dbe0e70f1899729830994f563684d96e9c0
Seena Rowhani <seenarowhani95@gmail.com>
2014-11-03 13:09:00 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 require(['app', 'Utils', 'Player','Canvas'], function(a,u,p,c){
-	if(/chrome/.test(window.navigator.userAgent.toLowerCase())){
-		a.init(u,p,c);
-		return;
-	}
-	document.writeln("Sorry, only working in Chrome");
-	document.writeln("(For now) Shh....");
-	return;
+	window.addEventListener('load', function(e){
+		if(/chrome/.test(window.navigator.userAgent.toLowerCase())){
+			a.init(u,p,c);
+			return;
+		}
+		document.writeln("Sorry, only working in Chrome");
+		document.writeln("(For now) Shh....");
+		return;	
+	});
+	
 });
